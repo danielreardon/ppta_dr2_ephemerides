51bcef0d5650b8648dbcee2e5bda34d0a1f8f843
Daniel Reardon <dreardon@swin.edu.au>
2020-05-11 23:39:15 +1000

    Added early-only datasets

--- partim/test.sh
+++ partim/test.sh
@@ -8,64 +8,7 @@ cd $dr1e_dir
 ls -1 | cut -d. -f1 | sort | uniq | grep -v "psrs" | grep -v "J0437-4715" > psrs.list # do J0437 manually
 cd ../..
 
-echo "REPLACING ALL CPSR2_20CM data, with dr1"
-foreach psr (`cat $dr1e_dir/psrs.list`)
-	
-	echo $psr
-	cp $dr2_dir/$psr.par $outdir/.
-	cp $dr2_dir/$psr.tim $outdir/.	
-
-	grep -vi "20cm_cpsr2" $dr2_dir/$psr.tim > $outdir/dr2_nocpsr2.tim
-	grep -vi "20cm_cpsr2" $dr2_dir/$psr.par | grep -v "NE_SW" | grep -v "START" | grep -v "FINISH" | grep -v "#" > $outdir/dr2_nocpsr2.par
-	sed -i 's/ -q / -group /g' $outdir/dr2_nocpsr2.tim	
-
-	grep -i "cpsr2" $dr1e_dir/$psr.tim | grep -i "20cm\|multi\|h-oh" | grep -v "T2E" > $outdir/extra.tim
-	grep -i	"cpsr2"	$dr1e_dir/$psr.par | grep -i "20cm\|multi\|h-oh"  > $outdir/extra.par
-	grep -i	"cpsr1"	$dr1e_dir/$psr.tim | grep -v "T2E" >> $outdir/extra.tim
-        grep -i "cpsr1" $dr1e_dir/$psr.par >> $outdir/extra.par
-	grep -i "fptm" $dr1e_dir/$psr.tim | grep -v "T2E" >> $outdir/extra.tim
-        grep -i "fptm" $dr1e_dir/$psr.par >> $outdir/extra.par
-	grep -i "_s21" $dr1e_dir/$psr.tim | grep -v "T2E" >> $outdir/extra.tim
-        grep -i "_s21" $dr1e_dir/$psr.par >> $outdir/extra.par
-	grep -i "_s22" $dr1e_dir/$psr.tim | grep -v "T2E" >> $outdir/extra.tim
-        grep -i "_s22" $dr1e_dir/$psr.par >> $outdir/extra.par
-	
-	sed -i 's/ -g / -x dr1 -group /g' $outdir/extra.tim
-	sed -i 's/ 7 -f / pks -f /g' $outdir/extra.tim
-	sed -i 's/ -g / -group /g' $outdir/extra.par
-
-	grep -v "B " $outdir/extra.tim > legacy.tim
-        grep "B " $outdir/extra.tim > withB.tim
-        rm $outdir/extra.tim
-        sed -i 's/ -x dr1 / -x dr1 -B 20CM /g' legacy.tim
-	cat withB.tim >> legacy.tim
-	mv legacy.tim $outdir/extra.tim
-	rm withB.tim
-
-	cat $outdir/extra.tim >> $outdir/dr2_nocpsr2.tim
-	cat $outdir/extra.par >> $outdir/dr2_nocpsr2.par
-
-	cd $outdir
-	tempo2 -f dr2_nocpsr2.par dr2_nocpsr2.tim -nofit -fit F0 -fit F1 -fit DM -fit FD1 -newpar
-	cd ..
-
-	mkdir -p $outdir/early_only
-	grep -v "TN" $outdir/new.par | grep -v "START" | grep -v "FINISH" > $outdir/early_only/$psr.par
-	touch $outdir/early_only/$psr.tim
-	echo "FORMAT 1" >> $outdir/early_only/$psr.tim
-	echo "MODE 1" >> $outdir/early_only/$psr.tim
-	cat $outdir/extra.tim >> $outdir/early_only/$psr.tim
-
-	mv $outdir/dr2_nocpsr2.tim $outdir/$psr.tim
-	mv $outdir/dr2_nocpsr2.par $outdir/$psr.par
-
-
-end
-
-rm $outdir/extra.tim
-rm $outdir/extra.par
-
-setenv outdir /fred/oz002/dreardon/ppta_dr2_ephemerides/partim/dr2e_cpsr2_prepend
+setenv outdir /fred/oz002/dreardon/ephemeris_ppta_dr2/partim/dr2e_cpsr2_prepend
 
 echo "PREPENDING DR1 CPSR2_20CM data, with dr1"
 foreach psr (`cat $dr1e_dir/psrs.list`)
