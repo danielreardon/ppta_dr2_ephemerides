9a33514f6c66ec812d8cbc72de67be2f8d52b929
Daniel Reardon <dreardon@swin.edu.au>
2020-06-24 12:42:07 +1000

    Added general2 outputs for making Shapiro delay plots

---
+++ publish_collection/plot_Shapiro.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/tcsh
+
+setenv dr2dir /fred/oz002/dreardon/ppta_dr2_ephemerides/publish_collection/dr2
+setenv dr2edir /fred/oz002/dreardon/ppta_dr2_ephemerides/publish_collection/dr2e/
+
+cd $dr2dir
+mkdir -p $dr2dir/shapiro/
+
+foreach parfile (`grep -l "SINI \|STIG " *.par | grep -v "kop_alt" | grep -v "1545\|2145"`)
+	echo $parfile
+	setenv psrname `echo $parfile | cut -d'.' -f1`
+	tempo2 -output general2 -f $parfile $psrname.tim -nobs 50000 -s "{file}\t{bat}\t{freq}\t{post}\t{err}\t{posttn}\t{tndm}\t{tndmerr}\t{tnrn}\t{tnrnerr}\t{tnchrom}\t{tnchromerr}\t{binphase}\n" > shapiro/$parfile.out
+	grep -v "H3 \|H4 \|STIG \|M2 " $parfile > temp.par
+	tempo2 -output general2 -f temp.par $psrname.tim -nobs 50000 -s "{file}\t{bat}\t{freq}\t{post}\t{err}\t{posttn}\t{tndm}\t{tndmerr}\t{tnrn}\t{tnrnerr}\t{tnchrom}\t{tnchromerr}\t{binphase}\n" > shapiro/$parfile.no_shapiro.out
+
+end
+
+rm temp.par
+
+cd ..
