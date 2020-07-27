7b9ff4c77b1037e1618751c041ab9896d66e6b56
Nithyanandan Thyagarajan <nithyanandan.t@gmail.com>
2020-07-27 16:16:25 +1000

    Add derive_PSRparms.py to repo, start deriving parms

--- parameterComparisonScripts/derive_PSRparms.py
+++ parameterComparisonScripts/derive_PSRparms.py
@@ -25,6 +25,12 @@ font = {'family' : 'normal',
 
 matplotlib.rc('font', **font)
 
+def is_valid(array):
+    """
+    Returns boolean array of values that are finite an not nan
+    """
+    return NP.isfinite(array)*(~NP.isnan(array))
+
 def distance_from_parallax(psrparms):
     if 'PX' in psrparms:
         if 'PX_ERR' in psrparms:
@@ -42,9 +48,12 @@ def distance_from_pbdot(psrparms):
 
     if ('ELAT' in psrparms) and ('ELONG' in psrparms):
         c = SkyCoord(psrparms['ELONG'], psrparms['ELAT'], frame=BarycentricTrueEcliptic, unit=(U.deg, U.deg))
+    elif ('BETA' in psrparms) and ('LAMBDA' in psrparms):
+        c = SkyCoord(psrparms['LAMBDA'], psrparms['BETA'], frame=BarycentricTrueEcliptic, unit=(U.deg, U.deg))
+    elif ('RAJ' in psrparms) and ('DECJ' in psrparms):
+        c = SkyCoord(psrparms['RAJ'] + ' ' + psrparms['DECJ'], unit=(U.hourangle, U.deg))
     else:
-        c = SkyCoord(psrparms['RAJ'] + ' ' + psrparms['DECJ'],
-                 unit=(U.hourangle, U.deg))
+        return -1
 
     #Convert to Galactic
 
@@ -53,7 +62,7 @@ def distance_from_pbdot(psrparms):
     bdeg = c.galactic.b.value
     sigb = 0.0
 
-    if not 'ecliptic' in datadir:
+    if (('ELAT' not in psrparms) or ('ELONG' not in psrparms)) and (('BETA' not in psrparms) or ('LAMBDA' not in psrparms)):
 
         #Convert to Ecliptic
         #ecliptic transformation:
@@ -63,36 +72,54 @@ def distance_from_pbdot(psrparms):
 
         #proper motion transformation:
 
+        if ('PMRA' not in psrparms) or ('PMDEC' not in psrparms):
+            return -1
+        
         pm = ICRS(ra=c.ra.deg*U.degree, dec=c.dec.deg*U.deg, pm_ra_cosdec = psrparms['PMRA']*U.mas/U.yr, pm_dec = psrparms['PMDEC']*U.mas/U.yr)
         pm_ecliptic = pm.transform_to(BarycentricTrueEcliptic)
         pmlat = pm_ecliptic.pm_lat.value
         pmlon = pm_ecliptic.pm_lon_coslat.value
     
-    if 'PBDOT' in psrparms:
+    if ('PBDOT' in psrparms) and ('PBDOT_ERR' in psrparms):
         pbdot_posterior = NP.random.normal(loc=psrparms["PBDOT"], scale=psrparms["PBDOT_ERR"], size=n_samples)  # observed
-        pbdot_grav = 0.0
-        if 'PX' in psrparms:
-            dkpc, sigd, dkpc16, dkpc84 = distance_from_parallax(psrparms)
+        if 'PSRJ' in psrparms:
+            psrname = psrparms['PSRJ']
+        elif 'PSR' in psrparms:
+            psrname = psrparms['PSR']
+
+        if psrname == 'J1909-3744':
+            pbdot_grav = -2.763*10**(-15)
+        else:
+            pbdot_grav = 0
+
+        if ('PX' in psrparms) and 'PX_ERR' in psrparms:
+            d_prior = 1/NP.random.normal(loc=psrparms["PX"], scale=psrparms["PX_ERR"], size=n_samples) # observed
+            dkpc = NP.median(d_prior[(d_prior > 0)*(d_prior < 100)])
+            sigd = dkpc*psrparms["PX_ERR"]/psrparms["PX"]
         else:
             dkpc = 1.0
             sigd = 0.2*dkpc
         pb = psrparms['PB']
         pb_err = psrparms['PB_ERR']
         if ('PMELAT' in psrparms) and ('PMELONG' in psrparms):
-        # if 'ecliptic' in datadir:
             pmelat = psrparms['PMELAT']
             pmelong = psrparms['PMELONG']
             pm_tot = NP.sqrt(pmelat**2 + pmelong**2)
             pm = pm_tot/(sec_per_year*rad_to_mas)
-        else:
+        elif ('PMBETA' in psrparms) and ('PMLAMBDA' in psrparms):
+            pmelat = psrparms['PMBETA']
+            pmelong = psrparms['PMLAMBDA']
+            pm_tot = NP.sqrt(pmelat**2 + pmelong**2)
+            pm = pm_tot/(sec_per_year*rad_to_mas)
+        elif ('PMRA' in psrparms) and ('PMDEC' in psrparms):
             pmra = psrparms['PMRA']
             pmdec = psrparms['PMDEC']
-            pm_tot = NP.sqrt(pmelat**2 + pmelong**2)
+            pm_tot = NP.sqrt(pmra**2 + pmdec**2)
             pm = pm_tot/(sec_per_year*rad_to_mas)
     
         # Expected Shklovskii and Galactic terms
 
-        Ex_pl =  GalDyNP.r.modelLb.Expl(ldeg, sigl, bdeg, sigb, dkpc, sigd) # excess term parallel to the Galactic plane
+        Ex_pl =  GalDynPsr.modelLb.Expl(ldeg, sigl, bdeg, sigb, dkpc, sigd) # excess term parallel to the Galactic plane
         Ex_z =  GalDynPsr.modelLb.Exz(ldeg, sigl, bdeg, sigb, dkpc, sigd) # excess term perpendicular to the Galactic plane
         errpl = NP.abs(0.03*Ex_pl) # 3% for circular Bovy et al. (2012)
         errz = NP.abs(0.1*Ex_z) # 10% for vertical Holmberg and Flynn (2004)
@@ -112,26 +139,37 @@ def distance_from_pbdot(psrparms):
     return -1
 
 def mass_from_psrparms(psrparms):
-    mtot = 0
-    if 'H3' in psrparms:
+    mtot2 = 0
+    if 'BINARY' in psrparms:
+        if ('A1' not in psrparms) or ('PB' not in psrparms):
+            return -1
+        mass_func = (4*NP.pi**2/FCNST.G) * (psrparms['A1']*FCNST.c)**3/(psrparms['PB']*86400)**2
+        mass_func = mass_func/M_sun
+
+    if (('H3' not in psrparms) or ('H3_ERR' not in psrparms)) and (('M2' not in psrparms) or ('M2_ERR' not in psrparms)):
+        return -1
+    
+    if ('H3' in psrparms) and ('H3_ERR' in psrparms):
         h3 = psrparms["H3"]*10**6
         h3_err = psrparms["H3_ERR"]*10**6
         h3_arr = NP.random.normal(loc=h3, scale=h3_err, size=n_samples)
-        if 'H4' in psrparms:
+        if ('H4' in psrparms) and ('H4_ERR' in psrparms):
             h4 = psrparms["H4"]*10**6
             h4_err = psrparms["H4_ERR"]*10**6
             h4_arr = NP.random.normal(loc=h4, scale=h4_err, size=n_samples)
-        elif 'STIG' in psrparms:
+        elif ('STIG' in psrparms) and ('STIG_ERR' in psrparms):
             stig = psrparms["STIG"]
             stig_err = psrparms["STIG_ERR"]
             stig_arr = NP.random.normal(loc=stig, scale=stig_err, size=n_samples)
             h4_arr = stig_arr * h3_arr
+        else:
+            return -1
         sini = 2 * h3_arr * h4_arr / ( h3_arr**2 + h4_arr**2 )
         m2 = h3_arr**4 / h4_arr**3  # in us
         m2 = m2/(Tsun*10**6)
-    elif 'M2' in psrparms:
+    elif ('M2' in psrparms) and ('M2_ERR' in psrparms):
         m2 = NP.random.normal(loc=psrparms["M2"], scale=psrparms["M2_ERR"], size=n_samples)
-        if 'KIN' in psrparms:
+        if ('KIN' in psrparms) and ('KIN_ERR' in psrparms):
             kin = NP.random.normal(loc=psrparms["KIN"], scale=psrparms["KIN_ERR"], size=n_samples)
             sini = NP.sin(kin*NP.pi/180)
             inc=kin
@@ -142,11 +180,11 @@ def mass_from_psrparms(psrparms):
         cut = NP.argwhere((m2 > mass_func) * (m2 < 10))
         m2 = m2[cut]
         sini = sini[cut]
-        inc = NP.arcsin(sini)*180/NP.pi
-        mtot = (m2 * sini)**3 / mass_func
-        mp = NP.sqrt(mtot[is_valid(mtot)*is_valid(m2)]) - m2[is_valid(mtot)*is_valid(m2)]
-        mp = mp[is_valid(mp)]
-        mtot = mtot[is_valid(mtot)]
-        inc = inc[is_valid(inc)]
-        
+    inc = NP.arcsin(sini)*180/NP.pi
+    mtot2 = (m2 * sini)**3 / mass_func
+    mp = NP.sqrt(mtot2[is_valid(mtot2)*is_valid(m2)]) - m2[is_valid(mtot2)*is_valid(m2)]
+    mp = mp[is_valid(mp)]
+    mtot = NP.sqrt(mtot2[is_valid(mtot2)])
+    inc = inc[is_valid(inc)]
+
     return {'M2': NP.median(m2), 'M2_std': NP.std(m2), 'M2_lolim': NP.percentile(m2, q=16.0), 'M2_uplim': NP.percentile(m2, q=84.0), 'Mpsr': NP.median(mp), 'Mpsr_std': NP.std(mp), 'Mpsr_lolim': NP.percentile(mp, q=16.0), 'Mpsr_uplim': NP.percentile(mp, q=84.0), 'Mtot': NP.median(mtot), 'Mtot_std': NP.std(mtot), 'Mtot_lolim': NP.percentile(mtot, q=16.0), 'Mtot_uplim': NP.percentile(mtot, q=84.0), 'inc': NP.median(inc), 'inc_std': NP.std(inc), 'inc_lolim': NP.percentile(inc, q=16.0), 'inc_uplim': NP.percentile(inc, q=84.0)}
