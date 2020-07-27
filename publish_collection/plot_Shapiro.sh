9cfd77811d3e29403d3d0d24e272167a295e3542
Daniel Reardon <dreardon@swin.edu.au>
2020-07-28 01:11:18 +1000

    Shapiro for 1125

--- publish_collection/plot_Shapiro.sh
+++ publish_collection/plot_Shapiro.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/tcsh
 
-setenv dr2dir /fred/oz002/dreardon/ppta_dr2_ephemerides/publish_collection/dr2
+setenv dr2dir /fred/oz002/dreardon/ppta_dr2_ephemerides/publish_collection/dr2/ecliptic/
 setenv dr2edir /fred/oz002/dreardon/ppta_dr2_ephemerides/publish_collection/dr2e/
 
 cd $dr2dir
 mkdir -p $dr2dir/shapiro/
 
-foreach parfile (`grep -l "SINI \|STIG " *.par | grep -v "kop_alt" | grep -v "1545\|2145"`)
+#foreach parfile (`grep -l "SINI \|STIG " *.par | grep -v "kop_alt" | grep -v "1545\|2145"`)
+foreach parfile (`grep -l "SINI \|STIG " *.par | grep "1125"`)
 	echo $parfile
 	setenv psrname `echo $parfile | cut -d'.' -f1`
 	tempo2 -output general2 -f $parfile $psrname.tim -nobs 50000 -s "{file}\t{bat}\t{freq}\t{pre}\t{post}\t{err}\t{posttn}\t{tndm}\t{tndmerr}\t{tnrn}\t{tnrnerr}\t{tnchrom}\t{tnchromerr}\t{binphase}\n" > shapiro/$parfile.out
