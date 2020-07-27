4f085929acb6c204d4cdbfafdb4d826be9a44e1d
Daniel Reardon <dreardon@swin.edu.au>
2020-07-28 01:17:53 +1000

    Changed 1125 Shapiro data

--- publish_collection/plot_Shapiro.sh
+++ publish_collection/plot_Shapiro.sh
@@ -9,10 +9,10 @@ mkdir -p $dr2dir/shapiro/
 #foreach parfile (`grep -l "SINI \|STIG " *.par | grep -v "kop_alt" | grep -v "1545\|2145"`)
 foreach parfile (`grep -l "SINI \|STIG " *.par | grep "1125"`)
 	echo $parfile
-	setenv psrname `echo $parfile | cut -d'.' -f1`
-	tempo2 -output general2 -f $parfile $psrname.tim -nobs 50000 -s "{file}\t{bat}\t{freq}\t{pre}\t{post}\t{err}\t{posttn}\t{tndm}\t{tndmerr}\t{tnrn}\t{tnrnerr}\t{tnchrom}\t{tnchromerr}\t{binphase}\n" > shapiro/$parfile.out
+	setenv psrname `echo $parfile | cut -d'.' -f1 | cut -d'_' -f1`
+	tempo2 -output general2 -f $parfile ../$psrname.tim -nobs 50000 -s "{file}\t{bat}\t{freq}\t{pre}\t{post}\t{err}\t{posttn}\t{tndm}\t{tndmerr}\t{tnrn}\t{tnrnerr}\t{tnchrom}\t{tnchromerr}\t{binphase}\n" > shapiro/$parfile.out
 	grep -v "H3 \|H4 \|STIG \|M2 " $parfile > temp.par
-	tempo2 -output general2 -f temp.par $psrname.tim -nobs 50000 -s "{file}\t{bat}\t{freq}\t{pre}\t{post}\t{err}\t{posttn}\t{tndm}\t{tndmerr}\t{tnrn}\t{tnrnerr}\t{tnchrom}\t{tnchromerr}\t{binphase}\n" > shapiro/$parfile.no_shapiro.out
+	tempo2 -output general2 -f temp.par ../$psrname.tim -nobs 50000 -s "{file}\t{bat}\t{freq}\t{pre}\t{post}\t{err}\t{posttn}\t{tndm}\t{tndmerr}\t{tnrn}\t{tnrnerr}\t{tnchrom}\t{tnchromerr}\t{binphase}\n" > shapiro/$parfile.no_shapiro.out
 
 end
 
