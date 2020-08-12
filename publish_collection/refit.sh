a827dbc0d25e93de092302ea35a7b656a86a7b92
Daniel Reardon <dreardon@swin.edu.au>
2020-08-12 14:15:20 +1000

    Getting ready to refit

---
+++ publish_collection/refit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/tcsh
+
+
+foreach psr (`ls -1 *.tim | cut -d'.' -f1`)
+	echo $psr
+	
+	foreach parfile (`ls -1 $psr*par`)
+		setenv parname `echo $parfile`
+
+		echo $parname
+		#echo "CLK_CORR_CHAIN pks2gps.dr2e.clk gps2utc.clk utc2tai.clk tai2tt_bipm2018.clk" >> $parname
+		tempo2 -f $parname $psr.tim -nobs 30000 -set NITS 2 -newpar
+		mv new.par $parname
+            	echo "CLK_CORR_CHAIN pks2gps.dr2e.clk gps2utc.clk utc2tai.clk tai2tt_bipm2018.clk" >> $parname
+		echo "wrote $parname"
+	end
+
+
+end
+
