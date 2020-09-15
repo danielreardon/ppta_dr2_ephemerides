4d7aecc2c535860363635d77f8f12ee5225a6f4b
Daniel Reardon <dreardon@swin.edu.au>
2020-09-16 09:34:34 +1000

    Pulsar output files

--- final/tempo2/refit.sh
+++ final/tempo2/refit.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/tcsh
 
-#foreach psr ( `ls -1 *.par | cut -d'.' -f1` )
-foreach psr ( `cat psrs.list` )
+foreach psr ( `ls -1 *.par | cut -d'.' -f1` )
+#foreach psr ( `cat psrs.list` )
 	echo $psr	
-	tempo2 -f $psr.par $psr.tim -nobs 35000 -newpar
-	mv new.par $psr.new.par
+	#tempo2 -f $psr.par $psr.tim -nobs 35000 -newpar
+	#mv new.par $psr.new.par
+	tempo2 -output general2 -f $psr.par $psr.tim -nobs 50000 -s "{file}\t{bat}\t{freq}\t{pre}\t{post}\t{err}\t{posttn}\t{tndm}\t{tndmerr}\t{tnrn}\t{tnrnerr}\t{tnchrom}\t{tnchromerr}\n" > $psr.out
 end
 
