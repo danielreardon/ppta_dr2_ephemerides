c91b33b4700b731817f0a269bc8c1f6b9f93d662
Daniel Reardon <dreardon@swin.edu.au>
2020-12-07 12:12:35 +1100

    J1909 Kopeikin fit with equatorial coordinates

--- final/tempo2/kopeikin/launch_temponest_J1909.sh
+++ final/tempo2/kopeikin/launch_temponest_J1909.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=tnest_J1909
-#SBATCH --output=/fred/oz002/dreardon/ppta_dr2_ephemerides/final/tempo2/kopeikin/tnest_J1909.out
+#SBATCH --output=/fred/oz002/dreardon/ppta_dr2_ephemerides/final/tempo2/kopeikin/tnest_eq_J1909.out
 #SBATCH --ntasks=8
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=5g
@@ -18,5 +18,5 @@ export TEMPO2=/fred/oz002/rshannon/tempo2
 export TEMPO2_CLOCK_DIR=/home/dreardon/tempo2/clock
 
 # cfile fits M2, KOM, KIN, and low-freq noise parameters
-srun /fred/oz002/rshannon/bin/tempo2 -gr temponest -f J1909-3744.par J1909-3744.tim -cfile J1909-3744.cfile
+srun /fred/oz002/rshannon/bin/tempo2 -gr temponest -f J1909-3744_eq.par J1909-3744.tim -cfile J1909-3744_eq.cfile
 
